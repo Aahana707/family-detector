8e1103cf2c94be4dc5ddcf4d48902456cedccfa7
Aahana707 <73531753+Aahana707@users.noreply.github.com>
2021-10-23 10:11:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -13,3 +13,26 @@ function take_snapshot(){
         document.getElementById("result").innerHTML = '<img id="capture_image" src="'+data_uri+'"/>';
     });
     }
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/PenhVmEw3/model.json', moldelLoaded);
+
+
+function moldelLoaded() {
+    console.log("Moldel Loaded!");
+}
+
+
+function check() {
+    img = document.getElementById('capture_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error,results){
+    if (error){
+     console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("results_object_name").innerHTML=results[0].label;
+        document.getElementById("rusults_object_accuracy").innerHTML=results[0].confidence.toFixed(3);
+    }
+}
